e28db9ebed8287f26e9520a6d91380afa9d8f293
Gabriela <gabriela@student.21-school.ru>
2025-03-13 18:14:14 +0300

    add run both servers

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"user-service/internal/api/grpc_server"
 	"user-service/internal/api/handlers"
 	"user-service/internal/api/routes"
@@ -36,29 +40,64 @@ func main() {
 	router := mux.NewRouter()
     routes.RegisterUserHandlers(router, userHandler)
 
-    // RunHttpServer(router)
-	RunGrpcServer(grpc_server.NewServer(userService))
+	var wg sync.WaitGroup
+
+	signalChan := make(chan os.Signal)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-signalChan
+		cancel()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		RunHttpServer(ctx, router)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		RunGrpcServer(ctx, grpc_server.NewServer(userService))
+	}()
+
+	wg.Wait()
 }
 
-func RunHttpServer(router *mux.Router) {
+func RunHttpServer(ctx context.Context, router *mux.Router) {
 	httpServer := &http.Server{
         Addr:    ":8080",
         Handler: router,
     }
 
+	go func() {
+		<-ctx.Done()
+		log.Println("Shutting down HTTP Server...")
+		httpServer.Shutdown(context.Background())
+	}()
+
     log.Println("Server run :8080...")
     log.Fatal(httpServer.ListenAndServe())
 }
 
-func RunGrpcServer(server pb.UserserviceServer) {
+func RunGrpcServer(ctx context.Context, server pb.UserserviceServer) {
 	lis, err := net.Listen("tcp", "localhost:5000")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	defer lis.Close()
+
 	var opts []grpc.ServerOption
-	
 	grpcServer := grpc.NewServer(opts...)
 	pb.RegisterUserserviceServer(grpcServer, server)
+
+	go func() {
+		<-ctx.Done()
+		log.Println("Shutting down gRPC Server...")
+		grpcServer.GracefulStop()
+	}()
 
 	log.Println("gRPC Server run :5000...")
 	grpcServer.Serve(lis)
